8bb7a5a46de16465d1e5c159d4d6660f30781222
Camilo Uribe <milouri23@gmail.com>
2017-11-14 16:13:04 -0500

    Add new tests and fixes in for... of statement

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function filterNegativeNumbers(numbers) {
     }
     
     const noNegativeNumbers = [];
-    for (number of numbers) {
+    for (const number of numbers) {
         if (number >= 0) {
             noNegativeNumbers.push(number);
         }
@@ -37,7 +37,7 @@ function mapNumbersIntoStrings(numbers) {
     }
     
     const stringOfNumbers = [];
-    for (number of numbers) {
+    for (const number of numbers) {
         stringOfNumbers.push(String(number)); 
     }
     return stringOfNumbers;
@@ -96,24 +96,29 @@ const prueba3 = [-1, -4, -2, -3, -5, -8, -14];
 console.log(generateRandomNumber());
 
 console.log(filterNegativeNumbers());
+console.log(filterNegativeNumbers([]));
 console.log(filterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
 console.log(filterNegativeNumbers(prueba1));
 console.log(filterNegativeNumbers(prueba2));
 console.log(filterNegativeNumbers(prueba3));
 
 console.log(functionalFilterNegativeNumbers());
+console.log(functionalFilterNegativeNumbers([]));
 console.log(functionalFilterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
 console.log(functionalFilterNegativeNumbers(prueba1));
 console.log(functionalFilterNegativeNumbers(prueba2));
 console.log(functionalFilterNegativeNumbers(prueba3));
 
 console.log(mapNumbersIntoStrings());
+console.log(mapNumbersIntoStrings([]));
 console.log(mapNumbersIntoStrings([-4, -6, -5, 0, 34, 54, -7]));
 
 console.log(functionalMapNumbersIntoStrings());
+console.log(functionalMapNumbersIntoStrings([]));
 console.log(functionalMapNumbersIntoStrings([-4, -6, -5, 0, 34, 54, -7]));
 
 console.log(printType(0));
+console.log(printType([]));
 console.log(printType('1'));
 console.log(printType(true));
 console.log(printType());
@@ -125,6 +130,7 @@ console.log(isPalindrome('madam'));
 console.log(isPalindrome('Madam'));
 console.log(isPalindrome('cocacola'));
 console.log(isPalindrome('anitalavalatina'));
+console.log(isPalindrome('anita lava la tina'));
 
 const student = new Person('John', 25);
 student.printName(); // "John"
