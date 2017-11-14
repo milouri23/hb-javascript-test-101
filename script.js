0ad1974bfc3d57106c9d5a3afc228f74e93de820
Camilo Uribe <milouri23@gmail.com>
2017-11-14 16:37:31 -0500

    Export all functions, remove manual tests

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //functions and classes declarations
-function generateRandomNumber() {
+export function generateRandomColor() {
     return {
         r: Math.floor(Math.random() * 256),
         g: Math.floor(Math.random() * 256),
@@ -7,7 +7,7 @@ function generateRandomNumber() {
     };
 }
 
-function filterNegativeNumbers(numbers) {
+export function filterNegativeNumbers(numbers) {
     if (typeof(numbers) === 'undefined') {
         return [];
     }
@@ -21,7 +21,7 @@ function filterNegativeNumbers(numbers) {
     return noNegativeNumbers;
 }
 
-function functionalFilterNegativeNumbers(numbers) {
+export function functionalFilterNegativeNumbers(numbers) {
     if (typeof(numbers) === 'undefined') {
         return [];
     }
@@ -31,7 +31,7 @@ function functionalFilterNegativeNumbers(numbers) {
     });
 }
 
-function mapNumbersIntoStrings(numbers) {
+export function mapNumbersIntoStrings(numbers) {
     if (typeof(numbers) === 'undefined') {
         return [];
     }
@@ -43,7 +43,7 @@ function mapNumbersIntoStrings(numbers) {
     return stringOfNumbers;
 }
 
-function functionalMapNumbersIntoStrings(numbers) {
+export function functionalMapNumbersIntoStrings(numbers) {
     if (typeof(numbers) === 'undefined') {
         return [];
     }
@@ -51,11 +51,11 @@ function functionalMapNumbersIntoStrings(numbers) {
     return numbers.map(String);
 }
 
-function printType(variable) {
+export function printType(variable) {
     return typeof(variable);
 }   
 
-function isPalindrome(word) {
+export function isPalindrome(word) {
     if (typeof(word) === 'undefined') {
         return false;
     }
@@ -63,7 +63,7 @@ function isPalindrome(word) {
     return word.toLowerCase().split("").reverse().join("") == word.toLowerCase();    
 }
 
-class Person {
+export class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -83,56 +83,7 @@ class Person {
 //     console.log(this.name);
 // };
 
-function printOutPersonAge(person) {
+export function printOutPersonAge(person) {
     console.log(person.age);
 }
 /* End of function and classes declaration */
-
-/* Testing */
-const prueba1 = [1, 3, 6, 8, 12, 15, 88, 10001, 33284];
-const prueba2 = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-const prueba3 = [-1, -4, -2, -3, -5, -8, -14];
-
-console.log(generateRandomNumber());
-
-console.log(filterNegativeNumbers());
-console.log(filterNegativeNumbers([]));
-console.log(filterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
-console.log(filterNegativeNumbers(prueba1));
-console.log(filterNegativeNumbers(prueba2));
-console.log(filterNegativeNumbers(prueba3));
-
-console.log(functionalFilterNegativeNumbers());
-console.log(functionalFilterNegativeNumbers([]));
-console.log(functionalFilterNegativeNumbers([-4, -6, -5, 0, 34, 54, -7]));
-console.log(functionalFilterNegativeNumbers(prueba1));
-console.log(functionalFilterNegativeNumbers(prueba2));
-console.log(functionalFilterNegativeNumbers(prueba3));
-
-console.log(mapNumbersIntoStrings());
-console.log(mapNumbersIntoStrings([]));
-console.log(mapNumbersIntoStrings([-4, -6, -5, 0, 34, 54, -7]));
-
-console.log(functionalMapNumbersIntoStrings());
-console.log(functionalMapNumbersIntoStrings([]));
-console.log(functionalMapNumbersIntoStrings([-4, -6, -5, 0, 34, 54, -7]));
-
-console.log(printType(0));
-console.log(printType([]));
-console.log(printType('1'));
-console.log(printType(true));
-console.log(printType());
-console.log(printType({a: '1', b: '2'}));
-
-console.log(isPalindrome());
-console.log(isPalindrome(""));
-console.log(isPalindrome('madam'));
-console.log(isPalindrome('Madam'));
-console.log(isPalindrome('cocacola'));
-console.log(isPalindrome('anitalavalatina'));
-console.log(isPalindrome('anita lava la tina'));
-
-const student = new Person('John', 25);
-student.printName(); // "John"
-printOutPersonAge(student); // 25
-/* End of testing */
