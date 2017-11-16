2e1d91d53ebed140aed29a30bf0b226e4b865315
Camilo Uribe Díaz <milouri23@gmail.com>
2017-11-16 06:59:59 -0500

    Add no deterministic test and format test to GenerateRandomColor()

--- script.test.js
+++ script.test.js
@@ -1,3 +1,4 @@
+
 import {
     generateRandomColor,
     filterNegativeNumbers,
@@ -7,29 +8,33 @@ import {
 } from './script.js'
 
 describe('generateRandomColor', () => {
-    const keys = ["r", "g", "b"]; 
-    const test_items = [];
-    for (let i = 0; i < 25; i++) {
-        test_items.push(generateRandomColor());
-    }
+    const keys = ['r', 'g', 'b']
+    const test_item = generateRandomColor();
 
-    test('should have items in the range [0, 255]', () => {
-        for (let test_item of test_items) {
-            for (let key of keys) {
-                expect(test_item[key])
-                    .toBeGreaterThanOrEqual(0);
-                expect(test_item[key])
-                    .toBeLessThanOrEqual(255);
-            }
-        }
+    test('should have only 3 keys in the next order: r, g, b', 
+    () => {
+        expect(Object.keys(test_item))
+            .toEqual(keys);            
     });
-    test('should have Integer items', () => {
-        for (let test_item of test_items) {
-            for (let key of keys) {    
-                expect(parseInt(test_item[key]) === test_item[key])
-                    .toBe(true);
+    test('should not have deterministic values', () => {
+        expect(test_item).not.toEqual(generateRandomColor());
+    });
+    test('should have values in the range [0, 255]', 
+        () => {
+            var test_item = null;
+            for (let i = 0; i < 85; i++) {
+                test_item = generateRandomColor();
+                for (const key of keys) {
+                    expect(test_item[key]).toBeGreaterThanOrEqual(0);
+                    expect(test_item[key]).toBeLessThanOrEqual(255);
+                }
             }
-        }    
+        });
+    test('should have Integer values', () => {
+        for (const key of keys) {    
+            expect(parseInt(test_item[key]) === test_item[key])
+                .toBeTruthy();
+        }
     });
 });
 
@@ -41,26 +46,26 @@ describe('filterNegativeNumbers', () => {
                 .toEqual([]);            
         });
     test("should return empty array if an array of negative numbers is given", 
-        () => {
-            expect(filterNegativeNumbers([-3, -4, -8, -2, -6, -7]))
-                .toEqual([]);
-            expect(functionalFilterNegativeNumbers([-3, -4, -8, -2, -6, -7]))
-                .toEqual([]);            
-        });
+    () => {
+        expect(filterNegativeNumbers([-3, -4, -8, -2, -6, -7]))
+            .toEqual([]);
+        expect(functionalFilterNegativeNumbers([-3, -4, -8, -2, -6, -7]))
+            .toEqual([]);            
+    });
     test("should return the full array if an array of no negative numbers is given", 
-        () => {
-            expect(filterNegativeNumbers([0, 2, 5, 7, 4, 3, 8, 9]))
-                .toEqual([0, 2, 5, 7, 4, 3, 8, 9]);
-            expect(functionalFilterNegativeNumbers([0, 2, 5, 7, 4, 3, 8, 9]))
-                .toEqual([0, 2, 5, 7, 4, 3, 8, 9]);
-        });
-    test("should return the full array if an array of no negative numbers is given", 
-        () => {
-            expect(filterNegativeNumbers([3, -3, 6, 8, -6, -8, 0]))
-                .toEqual([3, 6, 8, 0]);
-            expect(functionalFilterNegativeNumbers([3, -3, 6, 8, -6, -8, 0]))
-                .toEqual([3, 6, 8, 0]);
-        });  
+    () => {
+        expect(filterNegativeNumbers([0, 2, 5, 7, 4, 3, 8, 9]))
+            .toEqual([0, 2, 5, 7, 4, 3, 8, 9]);
+        expect(functionalFilterNegativeNumbers([0, 2, 5, 7, 4, 3, 8, 9]))
+            .toEqual([0, 2, 5, 7, 4, 3, 8, 9]);
+    });
+    test("should return the no negative numbers of the given array", 
+    () => {
+        expect(filterNegativeNumbers([3, -3, 6, 8, -6, -8, 0]))
+            .toEqual([3, 6, 8, 0]);
+        expect(functionalFilterNegativeNumbers([3, -3, 6, 8, -6, -8, 0]))
+            .toEqual([3, 6, 8, 0]);
+    });  
 });
 
 describe('mapNumbersIntoStrings', () => {
